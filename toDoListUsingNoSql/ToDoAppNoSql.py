0083500496d7e627fab0563dfd641aace4f9cc38
Asaddddd <mohammadasad455@gmail.com>
2018-09-06 03:29:23 -0700

    Get All Tasks

--- toDoListUsingNoSql/ToDoAppNoSql.py
+++ toDoListUsingNoSql/ToDoAppNoSql.py
@@ -7,19 +7,33 @@ app.config['MONGO_URI']='mongodb://m.asad:55226688shitwrongpasswordshit!@ds14759
 
 mongo = PyMongo(app)
 
-@app.route('/add')
-def addTasks():
-    insertQuery=mongo.db.tasks
-    insertQuery.insert({'Title':'FlaskTask', 'Description':'TodoRESTful API - A TDD Approach to Building an API using NoSQL Database', 'Done':0})
-    insertQuery.insert({'Title': 'JavaScriptTask',
-                        'Description': 'Complete JS 40 chapters from "A Smarter Way To Learn JavaScript" ',
-                        'Done': 0})
-    insertQuery.insert({'Title': 'Interview Questions',
-                        'Description': 'Preparation for Job Interviews',
-                        'Done': 0})
-    insertQuery.insert({'Title': 'Tennis',
-                        'Description': 'Play tennis regularly, Health Goals',
-                        'Done': 1})
-    return 'Added'
+## Added Tasks to Database
+##@app.route('/add')
+#def addTasks():
+#    insertQuery=mongo.db.tasks
+#    insertQuery.insert({'Title':'FlaskTask', 'Description':'TodoRESTful API - A TDD Approach to Building an API using NoSQL Database', 'Done':0})
+#    insertQuery.insert({'Title': 'JavaScriptTask',
+#                        'Description': 'Complete JS 40 chapters from "A Smarter Way To Learn JavaScript" ',
+#                        'Done': 0})
+#    insertQuery.insert({'Title': 'Interview Questions',
+#                      'Description': 'Preparation for Job Interviews',
+#                      'Done': 0})
+#    insertQuery.insert({'Title': 'Tennis',
+#                        'Description': 'Play tennis regularly, Health Goals','Done': 1})
+#    return 'Added'
+
+@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
+def getAllTasks():
+    tasks=mongo.db.tasks
+    output=[]
+
+    for q in tasks.find():
+        output.append({'Title':q['Title'], 'Description':q['Description'], 'Done':q['Done']})
+
+    return jsonify({'result': output})
+
+
+
+
 
 app.run(debug=True)
